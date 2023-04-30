adb5d3c00fb9aabb27f865465cfd52145c9506b7
JaeHyeokSeong <seong5763@gmail.com>
2023-05-01 03:33:29 +1000

    3-1_operators

---
+++ pythonBasic/week3/3-1_operators/main.py
@@ -0,0 +1,17 @@
+print(1 + 1)
+print(10 / 3)
+print(10 // 3) # 몫만 구하고 싶을 때에는 // 사용
+print(10 % 3)
+print(2**3)
+
+print(3 == 3)
+print(3 != 4)
+print(not (3 != 4)) # not 은 boolean 값을 반대로 할때 사용되어 진다
+
+print((3 > 0) & (3 < 5))
+print((3 > 0) and (3 < 5))
+
+print((3 > 0) | (3 > 5))
+print((3 > 0) or (3 > 5))
+
+print(3 < 5 < 7)
