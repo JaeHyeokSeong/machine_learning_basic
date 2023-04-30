933b8d0c9032c95e456a47d55e56b7ff12f635d2
JaeHyeokSeong <seong5763@gmail.com>
2023-05-01 03:09:00 +1000

    2-4_variable

---
+++ pythonBasic/week2/2-4_variable/main.py
@@ -0,0 +1,12 @@
+# 애완동물을 소개해 주세요
+
+animal = "고양이"
+name = "연탄이"
+age = 4
+isAdult = True
+
+print("우리집 " + animal + "는 " + name + " 입니다. 그리고 나이는 " + str(age) + " 살 입니다. 따라서 어른: " + str(isAdult) + " 입니다.")
+
+# , 를 사용해서 출력하는 방식
+print("우리집", animal, "는", age, "입니다.")
+
