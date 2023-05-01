a886c3408138d74aec5c84540c1ec5f280d291a9
JaeHyeokSeong <seong5763@gmail.com>
2023-05-02 04:53:24 +1000

    3-3_function

---
+++ pythonBasic/week3/3-3_function/main.py
@@ -0,0 +1,22 @@
+# absolute
+print(abs(-10))
+
+# power
+print(pow(4, 2))
+
+print(max(10, 20, 30, 40))
+
+print(min(10, 5, 2))
+
+print(round(3.14))
+
+print(round(4.6))
+
+# math library 에서 모든것을 다 import 한다
+from math import *
+
+print(floor(4.99))
+
+print(ceil(4.99))
+
+print(sqrt(100))
