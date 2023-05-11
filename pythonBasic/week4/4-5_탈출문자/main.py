2bfdce3dda5ca9071562e7ac3193a4608a88a1e4
JaeHyeokSeong <seong5763@gmail.com>
2023-05-12 00:12:26 +1000

    4-5_code

---
+++ pythonBasic/week4/4-5_탈출문자/main.py
@@ -0,0 +1,12 @@
+print("hello\nworld")
+
+print("I am \"jae\"")
+
+url = "/Users/jaehyeokseong/Desktop/jae hyeok document/python_workspace/pythonBasic/week4/4-5_탈출문자"
+print(url)
+
+window_url = "C:\\Users\\JaeHyeokSeong"
+
+print(window_url)
+
+print("hello\tworld")
