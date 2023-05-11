d6fe99773bd0dd48dd5410eb8b4ffe770d1f23f6
JaeHyeokSeong <seong5763@gmail.com>
2023-05-11 23:43:17 +1000

    4-3_code

---
+++ pythonBasic/week4/4-3_문자열처리함수/main.py
@@ -0,0 +1,16 @@
+txt = "python is amazing"
+
+print(txt)
+
+print(len(txt))
+
+new_txt = txt.replace("python", "c++")
+
+print(txt)
+print(new_txt)
+
+print(new_txt.find("python"))
+print(new_txt.find("c++"))
+
+# print(new_txt.index("python"))
+print(new_txt.index("c++"))
