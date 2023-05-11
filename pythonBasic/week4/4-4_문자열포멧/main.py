e40f86b6df2d17288ee94272ad06079b4221ad23
JaeHyeokSeong <seong5763@gmail.com>
2023-05-12 00:00:26 +1000

    4-4

---
+++ pythonBasic/week4/4-4_문자열포멧/main.py
@@ -0,0 +1,19 @@
+a = 10
+b = 20
+
+print("age : " + str(a + b))
+
+print("my age is {}".format(23))
+
+print("my name is {} and age is {}".format("Jae", 23))
+
+print("my name is {0} and age is {1} ".format("Jae", 23))
+
+print("my name is {1} and age is {0} ".format("Jae", 23))
+
+print("my name is {name} and age is {age}".format(name="Jae", age=23))
+
+name = "Jae Hyeok Seong"
+age = 23
+
+print(f"my name is {name} and age is {age}")
