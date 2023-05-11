bf50313755c952137ccab8746354f2645736e844
JaeHyeokSeong <seong5763@gmail.com>
2023-05-11 23:29:17 +1000

    4-2_code

---
+++ pythonBasic/week4/4-2_슬라이싱/main.py
@@ -0,0 +1,20 @@
+jae = "011121-1234567"
+
+print(jae)
+
+print("gender : " + jae[7])
+
+print("year : " + jae[0:2])
+
+print("month : " + jae[2:4])
+
+print("date : " + jae[4:6])
+
+print("unique number : " + jae[7:14])
+
+print("unique number : " + jae[7:])
+
+# 뒤에서부터 카운팅 하는 방법
+print(jae[-7:])
+
+
