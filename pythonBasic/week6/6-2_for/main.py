770e08af48807f4de470cfbe67b6a8921752884d
JaeHyeokSeong <seong5763@gmail.com>
2023-05-23 01:54:20 +1000

    6-2_for_code

---
+++ pythonBasic/week6/6-2_for/main.py
@@ -0,0 +1,10 @@
+print("for loop practise")
+
+# for waitingNumber in range(1, 6):
+#     print(f"waiting number {waitingNumber}")
+
+customers = ["son", "hwan", "lee", "kim"]
+
+for customer in customers:
+    print(f"{customer} : coffee is ready!")
+    
