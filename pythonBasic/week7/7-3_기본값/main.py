444edb479ad13552494be88c90823c13a8fc9ea2
JaeHyeokSeong <seong5763@gmail.com>
2023-05-24 02:04:58 +1000

    7-3_기본값_code

---
+++ pythonBasic/week7/7-3_기본값/main.py
@@ -0,0 +1,9 @@
+print("7-3_기본값 practise\n")
+
+
+def profile(name="unkown", age=23, main_language="python"):
+    print(f"name : {name}, age : {age}, main language : {main_language}")
+
+
+profile("Jae Hyeok Seong")
+profile()
