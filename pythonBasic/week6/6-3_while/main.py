4c1353c91ea734baa946c7ef3da8d8e62725b66c
JaeHyeokSeong <seong5763@gmail.com>
2023-05-23 17:02:33 +1000

    while loop practise

---
+++ pythonBasic/week6/6-3_while/main.py
@@ -0,0 +1,11 @@
+print("while loop practise")
+
+customer = "son"
+person = "unknown"
+
+while customer != person:
+    print(f"{customer} 님 커피가 준비 되었습니다.")
+    person = input("안녕하세요 이름이 어떻게 되시나요? ")
+
+print(f"감사합니다 {customer} 고객님 ^^")
+
