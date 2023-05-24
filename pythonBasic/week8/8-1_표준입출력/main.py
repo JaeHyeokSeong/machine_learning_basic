a280e4031fbbbc457eab8dcf55f1d977107a98d2
JaeHyeokSeong <seong5763@gmail.com>
2023-05-24 19:12:59 +1000

    8-1_code

---
+++ pythonBasic/week8/8-1_표준입출력/main.py
@@ -0,0 +1,13 @@
+print("8-1 표준입출력 practise\n")
+
+menus = {"비빔밥": 20, "불고기": 30, "된장 찌개": 20}
+
+# for menu, price in menus.items():
+#     print(menu.ljust(7), str(price).rjust(5), sep=":")
+
+# 은행 대기 순번표
+# for number in range(1, 21):
+#     print(f"대기 번호 : {str(number).zfill(3)}")
+
+answer = input("enter anything: ")
+print(type(answer), answer, sep=" : ")
